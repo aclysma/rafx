12c8196c0915848e86ac9092ade05cf610c89763
Philip Degarmo <aclysma@gmail.com>
2020-05-17 09:33:10 -0700

    Add fence to lib.rs

--- renderer-shell-vulkan/src/lib.rs
+++ renderer-shell-vulkan/src/lib.rs
@@ -68,6 +68,8 @@ pub use coordinates::Size;
 pub use coordinates::LogicalSize;
 pub use coordinates::PhysicalSize;
 
+mod fence;
+
 /// Used to select which PresentMode is preferred. Some of this is hardware/platform dependent and
 /// it's a good idea to read the Vulkan spec.
 ///
