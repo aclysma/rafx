4c09f1ac1175a594e73a088f855635997ebb2cb0
Philip Degarmo <aclysma@gmail.com>
2020-11-01 07:52:58 -0800

    Compile shaders with debug info

--- assets/shaders/compile.sh
+++ assets/shaders/compile.sh
@@ -1,25 +1,25 @@
 
 export PATH=~/dev/sdk/vulkansdk-macos-1.2.148.1/macOS/bin/glslc:$PATH
 
-glslc sprite.vert -o sprite.vert.spv
-glslc sprite.frag -o sprite.frag.spv
+glslc sprite.vert -o sprite.vert.spv -g
+glslc sprite.frag -o sprite.frag.spv -g
 
-glslc mesh.vert -o mesh.vert.spv
-glslc mesh.frag -o mesh.frag.spv
+glslc mesh.vert -o mesh.vert.spv -g
+glslc mesh.frag -o mesh.frag.spv -g
 
-glslc mesh_shadow_map.vert -o mesh_shadow_map.vert.spv
+glslc mesh_shadow_map.vert -o mesh_shadow_map.vert.spv -g
 
-glslc debug.vert -o debug.vert.spv
-glslc debug.frag -o debug.frag.spv
+glslc debug.vert -o debug.vert.spv -g
+glslc debug.frag -o debug.frag.spv -g
 
-glslc bloom_extract.vert -o bloom_extract.vert.spv
-glslc bloom_extract.frag -o bloom_extract.frag.spv
+glslc bloom_extract.vert -o bloom_extract.vert.spv -g
+glslc bloom_extract.frag -o bloom_extract.frag.spv -g
 
-glslc bloom_blur.vert -o bloom_blur.vert.spv
-glslc bloom_blur.frag -o bloom_blur.frag.spv
+glslc bloom_blur.vert -o bloom_blur.vert.spv -g
+glslc bloom_blur.frag -o bloom_blur.frag.spv -g
 
-glslc bloom_combine.vert -o bloom_combine.vert.spv
-glslc bloom_combine.frag -o bloom_combine.frag.spv
+glslc bloom_combine.vert -o bloom_combine.vert.spv -g
+glslc bloom_combine.frag -o bloom_combine.frag.spv -g
 
-glslc imgui.vert -o imgui.vert.spv
-glslc imgui.frag -o imgui.frag.spv
+glslc imgui.vert -o imgui.vert.spv -g
+glslc imgui.frag -o imgui.frag.spv -g
