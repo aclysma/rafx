508c503bc23a2fecea71b93745d1e8d2c4f81e7c
David Von Derau <dvd@davidvonderau.com>
2021-03-30 18:06:05 -0700

    `update` just calls `schedule`.

--- demo/src/scenes/many_sprites_scene.rs
+++ demo/src/scenes/many_sprites_scene.rs
@@ -11,7 +11,7 @@ use crate::time::TimeState;
 use crate::RenderOptions;
 use glam::{Quat, Vec2, Vec3};
 use legion;
-use legion::{IntoQuery, Read, Resources, Schedule, SystemBuilder, World};
+use legion::{IntoQuery, Read, Resources, Schedule, SystemBuilder, World, Write};
 use rafx::assets::distill_impl::AssetResource;
 use rafx::assets::ImageAsset;
 use rafx::distill::loader::handle::Handle;
@@ -22,12 +22,60 @@ use rand::Rng;
 
 const CAMERA_SPEED: f32 = 1000.0;
 
-pub(super) struct ManySpritesScene {
-    sprite_count: usize,
-    font: Handle<FontAsset>,
-    schedule: Schedule,
-    position: Transform,
+#[derive(Clone, Copy)]
+struct CameraComponent {
+    position: TransformComponent,
     up: Vec3,
+}
+
+#[derive(Clone)]
+struct TextComponent {
+    text: String,
+    font: Handle<FontAsset>,
+}
+
+#[derive(Clone, Copy)]
+struct TransformComponent {
+    pub translation: Vec3,
+    pub rotation: Quat,
+    pub scale: Vec3,
+}
+
+impl TransformComponent {
+    pub fn rotate(
+        &mut self,
+        rotation: Quat,
+    ) {
+        self.rotation *= rotation;
+    }
+
+    pub fn mul_transform(
+        &self,
+        transform: TransformComponent,
+    ) -> Self {
+        let translation = self.mul_vec3(transform.translation);
+        let rotation = self.rotation * transform.rotation;
+        let scale = self.scale * transform.scale;
+        TransformComponent {
+            translation,
+            rotation,
+            scale,
+        }
+    }
+
+    pub fn mul_vec3(
+        &self,
+        mut value: Vec3,
+    ) -> Vec3 {
+        value = self.rotation * value;
+        value = self.scale * value;
+        value += self.translation;
+        value
+    }
+}
+
+pub(super) struct ManySpritesScene {
+    schedule: Schedule,
 }
 
 impl ManySpritesScene {
@@ -56,6 +104,17 @@ impl ManySpritesScene {
         let half_x = (map_size.x() / 2.0) as i32;
         let half_y = (map_size.y() / 2.0) as i32;
 
+        let update_camera_system = SystemBuilder::new("update_camera")
+            .read_resource::<TimeState>()
+            .write_resource::<ViewportsResource>()
+            .with_query(<Write<CameraComponent>>::query())
+            .build(move |_, world, (time_state, viewports_resource), queries| {
+                profiling::scope!("update_camera_system");
+                for camera in queries.iter_mut(world) {
+                    update_main_view_2d(camera, time_state, viewports_resource);
+                }
+            });
+
         let update_render_node_system = SystemBuilder::new("update_render_node")
             .read_resource::<TimeState>()
             .write_resource::<SpriteRenderNodeSet>()
@@ -70,8 +129,26 @@ impl ManySpritesScene {
                 }
             });
 
+        let print_text_system = SystemBuilder::new("print_text")
+            .write_resource::<TextResource>()
+            .with_query(<Read<TextComponent>>::query())
+            .build(move |_, world, text_resource, queries| {
+                profiling::scope!("print_text_system");
+                for text in queries.iter_mut(world) {
+                    text_resource.add_text(
+                        text.text.clone(),
+                        glam::Vec3::new(25.0, 25.0, 0.0),
+                        &text.font,
+                        40.0,
+                        glam::Vec4::new(1.0, 0.0, 0.0, 1.0),
+                    );
+                }
+            });
+
         let schedule = Schedule::builder()
+            .add_system(update_camera_system)
             .add_system(update_render_node_system)
+            .add_system(print_text_system)
             .build();
 
         let mut sprite_count = 0 as usize;
@@ -126,136 +203,21 @@ impl ManySpritesScene {
 
         const CAMERA_Z: f32 = 1000.0;
 
-        let position = Transform {
-            translation: Vec3::new(0., 0., CAMERA_Z),
-            rotation: Quat::from_rotation_x(0.),
-            scale: Vec3::new(1., 1., 1.),
-        };
+        world.push((CameraComponent {
+            position: TransformComponent {
+                translation: Vec3::new(0., 0., CAMERA_Z),
+                rotation: Quat::from_rotation_x(0.),
+                scale: Vec3::new(1., 1., 1.),
+            },
+            up: Vec3::new(0., 1., 0.),
+        },));
 
-        let up = Vec3::new(0., 1., 0.);
-
-        ManySpritesScene {
-            sprite_count,
-            schedule,
+        world.push((TextComponent {
+            text: format!("Sprite Count: {}", sprite_count),
             font,
-            position,
-            up,
-        }
-    }
+        },));
 
-    #[profiling::function]
-    fn update_main_view_2d(
-        &mut self,
-        time: &TimeState,
-        viewports_resource: &mut ViewportsResource,
-    ) {
-        let main_camera_render_phase_mask = RenderPhaseMaskBuilder::default()
-            .add_render_phase::<DepthPrepassRenderPhase>()
-            .add_render_phase::<OpaqueRenderPhase>()
-            .add_render_phase::<TransparentRenderPhase>()
-            .add_render_phase::<UiRenderPhase>()
-            .build();
-
-        // Round to a whole number
-
-        self.position
-            .rotate(Quat::from_rotation_z(time.previous_update_dt() * 0.75));
-        self.position = self.position.mul_transform(Transform {
-            translation: Vec3::new(1., 0., 0.) * CAMERA_SPEED * time.previous_update_dt(),
-            rotation: Quat::from_rotation_x(0.),
-            scale: Vec3::new(1., 1., 1.),
-        });
-
-        let mut eye = self.position.translation;
-
-        let mut transform = Transform {
-            translation: eye,
-            rotation: Quat::from_rotation_x(0.),
-            scale: Vec3::new(1., 1., 1.),
-        };
-
-        transform.rotate(Quat::from_rotation_z(time.previous_update_dt() / 2.0));
-
-        self.up = transform.mul_vec3(self.up);
-
-        let half_width = viewports_resource.main_window_size.width as f32 / 2.0;
-        let half_height = viewports_resource.main_window_size.height as f32 / 2.0;
-
-        //
-        // This logic ensures pixel-perfect rendering when we have odd-sized width/height dimensions.
-        // We also need to round x/y to whole numbers to render pixel-perfect
-        //
-        if viewports_resource.main_window_size.width % 2 != 0 {
-            eye.set_x(eye.x().round() + 0.5);
-        } else {
-            eye.set_x(eye.x().round());
-        }
-
-        if viewports_resource.main_window_size.height % 2 != 0 {
-            eye.set_y(eye.y().round() + 0.5);
-        } else {
-            eye.set_y(eye.y().round());
-        }
-
-        let view = glam::Mat4::look_at_rh(eye, Vec3::new(0., 0., 0.), self.up);
-
-        let proj = glam::Mat4::orthographic_rh(
-            -half_width,
-            half_width,
-            -half_height,
-            half_height,
-            2000.0,
-            0.0,
-        );
-
-        viewports_resource.main_view_meta = Some(RenderViewMeta {
-            eye_position: eye,
-            view,
-            proj,
-            depth_range: RenderViewDepthRange::new_infinite_reverse(0.0),
-            render_phase_mask: main_camera_render_phase_mask,
-            debug_name: "main".to_string(),
-        });
-    }
-}
-
-#[derive(Clone, Copy)]
-struct Transform {
-    pub translation: Vec3,
-    pub rotation: Quat,
-    pub scale: Vec3,
-}
-
-impl Transform {
-    pub fn rotate(
-        &mut self,
-        rotation: Quat,
-    ) {
-        self.rotation *= rotation;
-    }
-
-    pub fn mul_transform(
-        &self,
-        transform: Transform,
-    ) -> Self {
-        let translation = self.mul_vec3(transform.translation);
-        let rotation = self.rotation * transform.rotation;
-        let scale = self.scale * transform.scale;
-        Transform {
-            translation,
-            rotation,
-            scale,
-        }
-    }
-
-    pub fn mul_vec3(
-        &self,
-        mut value: Vec3,
-    ) -> Vec3 {
-        value = self.rotation * value;
-        value = self.scale * value;
-        value += self.translation;
-        value
+        ManySpritesScene { schedule }
     }
 }
 
@@ -265,23 +227,82 @@ impl super::TestScene for ManySpritesScene {
         world: &mut World,
         resources: &mut Resources,
     ) {
-        {
-            let time_state = resources.get::<TimeState>().unwrap();
-            let mut viewports_resource = resources.get_mut::<ViewportsResource>().unwrap();
-            self.update_main_view_2d(&time_state, &mut *viewports_resource);
-        }
-
         self.schedule.execute(world, resources);
-
-        {
-            let mut text_resource = resources.get_mut::<TextResource>().unwrap();
-            text_resource.add_text(
-                format!("Sprite Count: {}", self.sprite_count),
-                glam::Vec3::new(25.0, 25.0, 0.0),
-                &self.font,
-                40.0,
-                glam::Vec4::new(1.0, 0.0, 0.0, 1.0),
-            );
-        }
     }
+}
+
+#[profiling::function]
+fn update_main_view_2d(
+    camera: &mut CameraComponent,
+    time: &TimeState,
+    viewports_resource: &mut ViewportsResource,
+) {
+    let main_camera_render_phase_mask = RenderPhaseMaskBuilder::default()
+        .add_render_phase::<DepthPrepassRenderPhase>()
+        .add_render_phase::<OpaqueRenderPhase>()
+        .add_render_phase::<TransparentRenderPhase>()
+        .add_render_phase::<UiRenderPhase>()
+        .build();
+
+    // Round to a whole number
+
+    camera
+        .position
+        .rotate(Quat::from_rotation_z(time.previous_update_dt() * 0.75));
+    camera.position = camera.position.mul_transform(TransformComponent {
+        translation: Vec3::new(1., 0., 0.) * CAMERA_SPEED * time.previous_update_dt(),
+        rotation: Quat::from_rotation_x(0.),
+        scale: Vec3::new(1., 1., 1.),
+    });
+
+    let mut eye = camera.position.translation;
+
+    let mut transform = TransformComponent {
+        translation: eye,
+        rotation: Quat::from_rotation_x(0.),
+        scale: Vec3::new(1., 1., 1.),
+    };
+
+    transform.rotate(Quat::from_rotation_z(time.previous_update_dt() / 2.0));
+
+    camera.up = transform.mul_vec3(camera.up);
+
+    let half_width = viewports_resource.main_window_size.width as f32 / 2.0;
+    let half_height = viewports_resource.main_window_size.height as f32 / 2.0;
+
+    //
+    // This logic ensures pixel-perfect rendering when we have odd-sized width/height dimensions.
+    // We also need to round x/y to whole numbers to render pixel-perfect
+    //
+    if viewports_resource.main_window_size.width % 2 != 0 {
+        eye.set_x(eye.x().round() + 0.5);
+    } else {
+        eye.set_x(eye.x().round());
+    }
+
+    if viewports_resource.main_window_size.height % 2 != 0 {
+        eye.set_y(eye.y().round() + 0.5);
+    } else {
+        eye.set_y(eye.y().round());
+    }
+
+    let view = glam::Mat4::look_at_rh(eye, Vec3::new(0., 0., 0.), camera.up);
+
+    let proj = glam::Mat4::orthographic_rh(
+        -half_width,
+        half_width,
+        -half_height,
+        half_height,
+        2000.0,
+        0.0,
+    );
+
+    viewports_resource.main_view_meta = Some(RenderViewMeta {
+        eye_position: eye,
+        view,
+        proj,
+        depth_range: RenderViewDepthRange::new_infinite_reverse(0.0),
+        render_phase_mask: main_camera_render_phase_mask,
+        debug_name: "main".to_string(),
+    });
 }
--- demo/src/scenes/rafxmark_scene.rs
+++ demo/src/scenes/rafxmark_scene.rs
@@ -11,6 +11,7 @@ use crate::time::TimeState;
 use crate::RenderOptions;
 use glam::{Quat, Vec2, Vec3};
 use legion;
+use legion::systems::CommandBuffer;
 use legion::{IntoQuery, Read, Resources, Schedule, SystemBuilder, World, Write};
 use rafx::assets::distill_impl::AssetResource;
 use rafx::assets::ImageAsset;
@@ -37,17 +38,30 @@ struct BodyComponent {
     velocity: Vec3,
 }
 
-pub(super) struct RafxmarkScene {
-    is_left_button_down: bool,
-    sprite_count: usize,
+#[derive(Clone)]
+struct TextComponent {
+    text: String,
     font: Handle<FontAsset>,
+}
+
+#[derive(Clone, Copy)]
+struct InputComponent {
+    is_left_mouse_button_down: bool,
+}
+
+#[derive(Clone)]
+struct SpriteSpawnerComponent {
+    sprite_count: usize,
     sprite_image: Handle<ImageAsset>,
+}
+
+pub(super) struct RafxmarkScene {
     schedule: Schedule,
 }
 
 impl RafxmarkScene {
     pub(super) fn new(
-        _world: &mut World,
+        world: &mut World,
         resources: &Resources,
     ) -> Self {
         let mut render_options = resources.get_mut::<RenderOptions>().unwrap();
@@ -62,6 +76,45 @@ impl RafxmarkScene {
             let asset_resource = resources.get::<AssetResource>().unwrap();
             asset_resource.load_asset_path::<FontAsset, _>("fonts/mplus-1p-regular.ttf")
         };
+
+        let update_camera_system = SystemBuilder::new("update_camera")
+            .write_resource::<ViewportsResource>()
+            .build(move |_, _, viewports_resource, _| {
+                profiling::scope!("update_camera_system");
+                update_main_view_2d(viewports_resource);
+            });
+
+        let sprite_spawner_system = SystemBuilder::new("sprite_spawner")
+            .read_resource::<TimeState>()
+            .write_resource::<SpriteRenderNodeSet>()
+            .write_resource::<DynamicVisibilityNodeSet>()
+            .with_query(<(
+                Read<InputComponent>,
+                Write<SpriteSpawnerComponent>,
+                Write<TextComponent>,
+            )>::query())
+            .build(
+                move |commands,
+                      world,
+                      (time, sprite_render_nodes, dynamic_visibility_node_set),
+                      queries| {
+                    profiling::scope!("sprite_spawner_system");
+                    for (input, sprite_spawner, sprite_count_text) in queries.iter_mut(world) {
+                        if input.is_left_mouse_button_down {
+                            add_sprites(
+                                sprite_spawner,
+                                commands,
+                                time,
+                                sprite_render_nodes,
+                                dynamic_visibility_node_set,
+                            );
+                        }
+
+                        sprite_count_text.text =
+                            format!("Sprite Count: {}", sprite_spawner.sprite_count);
+                    }
+                },
+            );
 
         let gravity_system = SystemBuilder::new("gravity")
             .read_resource::<TimeState>()
@@ -120,84 +173,47 @@ impl RafxmarkScene {
                 }
             });
 
+        let print_text_system = SystemBuilder::new("print_text")
+            .write_resource::<TextResource>()
+            .with_query(<Read<TextComponent>>::query())
+            .build(move |_, world, text_resource, queries| {
+                profiling::scope!("print_text_system");
+                for text in queries.iter_mut(world) {
+                    text_resource.add_text(
+                        text.text.clone(),
+                        glam::Vec3::new(25.0, 25.0, 0.0),
+                        &text.font,
+                        40.0,
+                        glam::Vec4::new(1.0, 1.0, 1.0, 1.0),
+                    );
+                }
+            });
+
         let schedule = Schedule::builder()
+            .add_system(update_camera_system)
+            .add_system(sprite_spawner_system)
             .add_system(gravity_system)
             .add_system(velocity_system)
             .add_system(collision_system)
             .add_system(update_render_node_system)
+            .add_system(print_text_system)
             .build();
 
-        RafxmarkScene {
-            is_left_button_down: false,
-            sprite_count: 0,
-            schedule,
-            sprite_image,
-            font,
-        }
-    }
+        world.push((
+            TextComponent {
+                text: "".to_string(),
+                font,
+            },
+            InputComponent {
+                is_left_mouse_button_down: false,
+            },
+            SpriteSpawnerComponent {
+                sprite_count: 0,
+                sprite_image,
+            },
+        ));
 
-    fn add_sprites(
-        &mut self,
-        world: &mut World,
-        resources: &Resources,
-    ) {
-        let time = resources.get::<TimeState>().unwrap();
-
-        let spawn_count = (SPRITES_PER_SECOND as f32 * time.previous_update_dt()) as usize;
-
-        let mut rng = rand::thread_rng();
-        let tint = super::random_color(&mut rng);
-
-        let sprite_x = LEFT + HALF_SPRITE_SIZE;
-        let sprite_y = TOP - HALF_SPRITE_SIZE;
-
-        for count in 0..spawn_count {
-            let sprite_z = (self.sprite_count + count) as f32 * 0.00001;
-
-            let velocity = Vec3::new(
-                rand::random::<f32>() * MAX_VELOCITY - (MAX_VELOCITY * 0.5),
-                0.,
-                0.,
-            );
-
-            let position = Vec3::new(sprite_x, sprite_y, 100.0 + sprite_z as f32);
-
-            let alpha = 0.8;
-
-            let mut sprite_render_nodes = resources.get_mut::<SpriteRenderNodeSet>().unwrap();
-            let mut dynamic_visibility_node_set =
-                resources.get_mut::<DynamicVisibilityNodeSet>().unwrap();
-
-            let render_node = sprite_render_nodes.register_sprite(SpriteRenderNode {
-                position,
-                scale: Vec2::splat(SPRITE_SCALE),
-                rotation: Quat::from_rotation_z(0.0),
-                tint,
-                alpha,
-                image: self.sprite_image.clone(),
-            });
-
-            let aabb_info = DynamicAabbVisibilityNode {
-                handle: render_node.as_raw_generic_handle(),
-            };
-
-            let visibility_node = dynamic_visibility_node_set.register_dynamic_aabb(aabb_info);
-
-            let position_component = PositionComponent { position };
-            let sprite_component = SpriteComponent {
-                render_node,
-                visibility_node,
-                alpha,
-                image: self.sprite_image.clone(),
-            };
-            let body_component = BodyComponent { velocity };
-
-            world.extend(
-                (0..1).map(|_| (position_component, sprite_component.clone(), body_component)),
-            );
-        }
-
-        self.sprite_count += spawn_count;
+        RafxmarkScene { schedule }
     }
 }
 
@@ -207,49 +223,91 @@ impl super::TestScene for RafxmarkScene {
         world: &mut World,
         resources: &mut Resources,
     ) {
-        {
-            let mut viewports_resource = resources.get_mut::<ViewportsResource>().unwrap();
-            update_main_view_2d(&mut *viewports_resource);
-        }
-
-        if self.is_left_button_down {
-            self.add_sprites(world, resources);
-        }
-
         self.schedule.execute(world, resources);
-
-        {
-            let mut text_resource = resources.get_mut::<TextResource>().unwrap();
-            text_resource.add_text(
-                format!("Sprite Count: {}", self.sprite_count),
-                glam::Vec3::new(25.0, 25.0, 0.0),
-                &self.font,
-                40.0,
-                glam::Vec4::new(1.0, 1.0, 1.0, 1.0),
-            );
-        }
     }
 
     fn process_input(
         &mut self,
-        _world: &mut World,
+        world: &mut World,
         _resources: &Resources,
         event: Event,
     ) {
+        let mut query = <Write<InputComponent>>::query();
+        let input = query.iter_mut(world).last().unwrap();
         match event {
             Event::MouseButtonDown { mouse_btn, .. } => {
                 if mouse_btn == MouseButton::Left {
-                    self.is_left_button_down = true;
+                    input.is_left_mouse_button_down = true;
                 }
             }
             Event::MouseButtonUp { mouse_btn, .. } => {
                 if mouse_btn == MouseButton::Left {
-                    self.is_left_button_down = false;
+                    input.is_left_mouse_button_down = false;
                 }
             }
             _ => {}
         }
     }
+}
+
+fn add_sprites(
+    sprite_spawner: &mut SpriteSpawnerComponent,
+    commands: &mut CommandBuffer,
+    time: &TimeState,
+    sprite_render_nodes: &mut SpriteRenderNodeSet,
+    dynamic_visibility_node_set: &mut DynamicVisibilityNodeSet,
+) {
+    let spawn_count = (SPRITES_PER_SECOND as f32 * time.previous_update_dt()) as usize;
+
+    let mut rng = rand::thread_rng();
+    let tint = super::random_color(&mut rng);
+
+    let sprite_x = LEFT + HALF_SPRITE_SIZE;
+    let sprite_y = TOP - HALF_SPRITE_SIZE;
+
+    for count in 0..spawn_count {
+        let sprite_z = (sprite_spawner.sprite_count + count) as f32 * 0.00001;
+
+        let velocity = Vec3::new(
+            rand::random::<f32>() * MAX_VELOCITY - (MAX_VELOCITY * 0.5),
+            0.,
+            0.,
+        );
+
+        let position = Vec3::new(sprite_x, sprite_y, 100.0 + sprite_z as f32);
+
+        let alpha = 0.8;
+
+        let render_node = sprite_render_nodes.register_sprite(SpriteRenderNode {
+            position,
+            scale: Vec2::splat(SPRITE_SCALE),
+            rotation: Quat::from_rotation_z(0.0),
+            tint,
+            alpha,
+            image: sprite_spawner.sprite_image.clone(),
+        });
+
+        let aabb_info = DynamicAabbVisibilityNode {
+            handle: render_node.as_raw_generic_handle(),
+        };
+
+        let visibility_node = dynamic_visibility_node_set.register_dynamic_aabb(aabb_info);
+
+        let position_component = PositionComponent { position };
+        let sprite_component = SpriteComponent {
+            render_node,
+            visibility_node,
+            alpha,
+            image: sprite_spawner.sprite_image.clone(),
+        };
+        let body_component = BodyComponent { velocity };
+
+        commands.extend(
+            (0..1).map(move |_| (position_component, sprite_component.clone(), body_component)),
+        );
+    }
+
+    sprite_spawner.sprite_count += spawn_count;
 }
 
 #[profiling::function]
