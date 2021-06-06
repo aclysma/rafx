e4a6d7d6faca24c53fc9263515d48ee37eec8b1c
Philip Degarmo <aclysma@gmail.com>
2021-06-05 18:58:44 -0700

    Remove the test compute pipeline from the demo (so that GLES3 backend can run cleanly). (#156)

--- demo/src/render_graph_generator/mod.rs
+++ demo/src/render_graph_generator/mod.rs
@@ -31,8 +31,6 @@ mod bloom_combine_pass;
 
 mod ui_pass;
 
-mod compute_test;
-
 lazy_static::lazy_static! {
     pub static ref EMPTY_VERTEX_LAYOUT : VertexDataSetLayout = {
         VertexDataSetLayout::new(vec![], RafxPrimitiveTopology::TriangleList)
@@ -122,25 +120,6 @@ impl RenderGraphGenerator for DemoRenderGraphGenerator {
         let shadow_maps = shadow_map_pass::shadow_map_passes(&mut graph_context);
 
         let opaque_pass = opaque_pass::opaque_pass(&mut graph_context, depth_prepass, &shadow_maps);
-
-        {
-            let compute_test_pipeline = asset_manager
-                .committed_asset(&static_resources.compute_test)
-                .unwrap()
-                .compute_pipeline
-                .clone();
-
-            let compute_test_pass =
-                compute_test::compute_test_pass(&mut graph_context, &compute_test_pipeline);
-
-            let _out = graph_context.graph.read_storage_buffer(
-                opaque_pass.node,
-                compute_test_pass.position_buffer,
-                RenderGraphBufferConstraint {
-                    ..Default::default()
-                },
-            );
-        }
 
         let previous_pass_color = if graph_config.enable_hdr {
             let bloom_extract_material_pass = asset_manager
