69acfc98f90069a69a1e45bf4c7bf08da4e84d01
Zicklag <zicklag@katharostech.com>
2021-06-30 23:05:12 -0500

    Add Features to Docs.rs & Inline Re-exported Crates (#165)
    * Enable Cargo Features For Docs.rs Builds
    * Inline Re-exported Crate Documentation

--- rafx/src/lib.rs
+++ rafx/src/lib.rs
@@ -1,18 +1,19 @@
-//! NOTE: **docs.rs may not generate complete documentation for rafx because it does not enable any
-//! features in cargo when building the docs.** To generate complete documentation locally, run
-//! `cargo doc --no-deps --open` in the root of the crate.
-//!
-//! **Please see [additional documentation here](https://github.com/aclysma/rafx/blob/master/docs/index.md)**
+//! NOTE: **docs.rs may not generate complete documentation for rafx because it does not enable all
+//! of the features in cargo when building the docs.** To generate complete documentation locally,
+//! run `cargo doc --no-deps --open` in the root of the crate.
 //!
 //! Rafx is a multi-backend renderer that prioritizes performance, flexibility, and productivity. It
 //! optionally integrates with the [`distill`](https://github.com/amethyst/distill) asset
 //! pipeline to provide workflows and tools suitable for real-world projects with multidisciplinary
 //! teams.
 //!
+//! > **Note:** Additional documentation describing aspects of `rafx`'s design, usage, and architecture
+//! can be found on [GitHub](https://github.com/aclysma/rafx/blob/master/docs/index.md).
+//!
 //! This crate contains several layers:
 //!  * [`rafx_api`]: Low-level graphics API abstraction
-//!  * [`rafx_framework`]: Mid-level framework that eases resource management, lifetime handling, and
-//!     draw call dispatching
+//!  * [`rafx_framework`]: Mid-level framework that eases resource management, lifetime handling,
+//!     and draw call dispatching
 //!  * [`rafx_assets`]: Asset layer that integrates with the
 //!     [`distill`](https://github.com/amethyst/distill) asset pipeline
 //!      * NOTE: The published version in crates.io does not include rafx-assets as `distill` is not
@@ -20,19 +21,27 @@
 //!
 //! Rafx also provides tools for building shaders and packing assets.
 //!
-//! Rafx supports most mainstream platforms via `vulkan` and `metal` backends. Proprietary platforms
-//! can be supported by adding an additional backend.
+//! Rafx supports most mainstream platforms via `vulkan` and `metal` backends. OpenGL ES 2.0/3.0 backends
+//! are work-in-progress and they support less features due to API limitations ( see [backends] ).
+//! Proprietary platforms can be supported by adding an additional backend.
+//!
+//! [backends]: https://github.com/aclysma/rafx/tree/master/docs/api/backends
 
+#[doc(inline)]
 pub use rafx_base as base;
 
+#[doc(inline)]
 pub use rafx_api as api;
 
 #[cfg(feature = "assets")]
+#[doc(inline)]
 pub use rafx_assets as assets;
 #[cfg(feature = "assets")]
+#[doc(inline)]
 pub use rafx_assets::distill;
 
 #[cfg(feature = "framework")]
+#[doc(inline)]
 pub use rafx_framework as framework;
 #[cfg(feature = "framework")]
 pub use rafx_framework::declare_render_feature;
@@ -85,8 +94,11 @@ pub mod render_feature_mod_prelude {
     };
     pub use std::convert::TryInto;
 }
+
 #[cfg(feature = "framework")]
+#[doc(inline)]
 pub use rafx_visibility;
 
 #[cfg(feature = "renderer")]
+#[doc(inline)]
 pub use rafx_renderer as renderer;
