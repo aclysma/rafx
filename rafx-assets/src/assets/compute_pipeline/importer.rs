52ca24e68e7e01a1cba97b5c7fd3c3398c9cb339
Philip Degarmo <aclysma@gmail.com>
2023-11-06 21:47:40 -0800

    Remove deprecated importers

--- rafx-assets/src/assets/compute_pipeline/importer.rs
+++ rafx-assets/src/assets/compute_pipeline/importer.rs
@@ -14,59 +14,6 @@ use serde::{Deserialize, Serialize};
 use std::path::Path;
 use type_uuid::*;
 use uuid::Uuid;
-//
-// #[derive(TypeUuid, Serialize, Deserialize, Default)]
-// #[uuid = "4c1d6cd6-8fa3-4835-8985-f733a5ad3af0"]
-// pub struct ComputePipelineImporterState(Option<AssetUuid>);
-//
-// #[derive(TypeUuid)]
-// #[uuid = "a4cce767-da93-455d-92a8-c8b1f8e5f273"]
-// pub struct ComputePipelineImporter;
-// impl Importer for ComputePipelineImporter {
-//     fn version_static() -> u32
-//     where
-//         Self: Sized,
-//     {
-//         1
-//     }
-//
-//     fn version(&self) -> u32 {
-//         Self::version_static()
-//     }
-//
-//     type Options = ();
-//
-//     type State = ComputePipelineImporterState;
-//
-//     /// Reads the given bytes and produces assets.
-//     #[profiling::function]
-//     fn import(
-//         &self,
-//         _op: &mut ImportOp,
-//         source: &mut dyn Read,
-//         _options: &Self::Options,
-//         state: &mut Self::State,
-//     ) -> distill::importer::Result<ImporterValue> {
-//         let id = state
-//             .0
-//             .unwrap_or_else(|| AssetUuid(*uuid::Uuid::new_v4().as_bytes()));
-//         *state = ComputePipelineImporterState(Some(id));
-//
-//         let compute_pipeline_asset = ron::de::from_reader::<_, ComputePipelineAssetData>(source)?;
-//         log::trace!("IMPORTED COMPUTE PIPELINE:\n{:#?}", compute_pipeline_asset);
-//
-//         Ok(ImporterValue {
-//             assets: vec![ImportedAsset {
-//                 id,
-//                 search_tags: vec![],
-//                 build_deps: vec![],
-//                 load_deps: vec![],
-//                 build_pipeline: None,
-//                 asset_data: Box::new(compute_pipeline_asset),
-//             }],
-//         })
-//     }
-// }
 
 #[derive(TypeUuid, Default)]
 #[uuid = "a78c8ec9-11bf-45aa-886b-0080f3a52b40"]
--- rafx-plugins/src/assets/mesh_adv/importers/blender_model_importer.rs
+++ rafx-plugins/src/assets/mesh_adv/importers/blender_model_importer.rs
@@ -32,68 +32,6 @@ struct HydrateModelLodJsonFormat {
 struct HydrateModelJsonFormat {
     pub lods: Vec<HydrateModelLodJsonFormat>,
 }
-//
-// #[derive(TypeUuid, Serialize, Deserialize, Default)]
-// #[uuid = "1c6506cb-3bcf-49f3-9883-c36868da37c3"]
-// pub struct MeshAdvBlenderModelImporterState(Option<AssetUuid>);
-//
-// #[derive(TypeUuid)]
-// #[uuid = "ace983d5-5340-4872-a9e9-77f39f527f27"]
-// pub struct MeshAdvBlenderModelImporter;
-// impl Importer for MeshAdvBlenderModelImporter {
-//     fn version_static() -> u32
-//     where
-//         Self: Sized,
-//     {
-//         3
-//     }
-//
-//     fn version(&self) -> u32 {
-//         Self::version_static()
-//     }
-//
-//     type Options = ();
-//
-//     type State = MeshAdvBlenderModelImporterState;
-//
-//     /// Reads the given bytes and produces assets.
-//     #[profiling::function]
-//     fn import(
-//         &self,
-//         _op: &mut ImportOp,
-//         source: &mut dyn Read,
-//         _options: &Self::Options,
-//         state: &mut Self::State,
-//     ) -> distill::importer::Result<ImporterValue> {
-//         let id = state
-//             .0
-//             .unwrap_or_else(|| AssetUuid(*uuid::Uuid::new_v4().as_bytes()));
-//         *state = MeshAdvBlenderModelImporterState(Some(id));
-//
-//         let json_format: ModelJsonFormat = serde_json::from_reader(source)
-//             .map_err(|x| format!("Blender Material Import error: {:?}", x))?;
-//
-//         let mut lods = Vec::with_capacity(json_format.lods.len());
-//         for lod in json_format.lods {
-//             lods.push(ModelAdvAssetDataLod {
-//                 mesh: lod.mesh.clone(),
-//             });
-//         }
-//
-//         let asset_data = ModelAdvAssetData { lods };
-//
-//         Ok(ImporterValue {
-//             assets: vec![ImportedAsset {
-//                 id,
-//                 search_tags: vec![],
-//                 build_deps: vec![],
-//                 load_deps: vec![],
-//                 build_pipeline: None,
-//                 asset_data: Box::new(asset_data),
-//             }],
-//         })
-//     }
-// }
 
 #[derive(TypeUuid, Default)]
 #[uuid = "a97c46e9-1deb-4ca2-9f70-b4ce97a74cf2"]
